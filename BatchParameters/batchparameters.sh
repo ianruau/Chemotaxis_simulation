b954e10b30f040a3f30650599e467e89ace053f6
Ian Ruau <iir0001@auburn.edu>
2025-04-15 10:38:50 -0500

    Batchparmeters.sh updated

--- BatchParameters/batchparameters.sh
+++ BatchParameters/batchparameters.sh
@@ -33,52 +33,84 @@
 ../simulation.py --chi 30 --eigen_index 2 --m
 ./genhtml.py
 
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
+../simulation.py --chi 10 --eigen_index 2 --a
+../simulation.py --chi 10 --eigen_index 2 --a
+../simulation.py --chi 10 --eigen_index 2 --a
+../simulation.py --chi 10 --eigen_index 2 --a
+../simulation.py --chi 10 --eigen_index 2 --a
+../simulation.py --chi 10 --eigen_index 2 --a
 ./genhtml.py
 
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
+../simulation.py --chi 30 --eigen_index 2 --b
+../simulation.py --chi 30 --eigen_index 2 --b
+../simulation.py --chi 30 --eigen_index 2 --b
+../simulation.py --chi 30 --eigen_index 2 --b
+../simulation.py --chi 30 --eigen_index 2 --b
+../simulation.py --chi 30 --eigen_index 2 --b
 ./genhtml.py
 
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
+../simulation.py --chi 10 --eigen_index 2 --alpha
+../simulation.py --chi 10 --eigen_index 2 --alpha
+../simulation.py --chi 10 --eigen_index 2 --alpha
+../simulation.py --chi 10 --eigen_index 2 --alpha
+../simulation.py --chi 10 --eigen_index 2 --alpha
+../simulation.py --chi 10 --eigen_index 2 --alpha
 ./genhtml.py
 
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
+../simulation.py --chi 30 --eigen_index 2 --alpha
+../simulation.py --chi 30 --eigen_index 2 --alpha
+../simulation.py --chi 30 --eigen_index 2 --alpha
+../simulation.py --chi 30 --eigen_index 2 --alpha
+../simulation.py --chi 30 --eigen_index 2 --alpha
+../simulation.py --chi 30 --eigen_index 2 --alpha
 ./genhtml.py
 
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
-../simulation.py --chi 10 --eigen_index 2
+../simulation.py --chi 10 --eigen_index 2 --mu
+../simulation.py --chi 10 --eigen_index 2 --mu
+../simulation.py --chi 10 --eigen_index 2 --mu
+../simulation.py --chi 10 --eigen_index 2 --mu
+../simulation.py --chi 10 --eigen_index 2 --mu
+../simulation.py --chi 10 --eigen_index 2 --mu
 ./genhtml.py
 
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
-../simulation.py --chi 30 --eigen_index 2
+../simulation.py --chi 30 --eigen_index 2 --mu
+../simulation.py --chi 30 --eigen_index 2 --mu
+../simulation.py --chi 30 --eigen_index 2 --mu
+../simulation.py --chi 30 --eigen_index 2 --mu
+../simulation.py --chi 30 --eigen_index 2 --mu
+../simulation.py --chi 30 --eigen_index 2 --mu
+./genhtml.py
+
+../simulation.py --chi 10 --eigen_index 2 --nu
+../simulation.py --chi 10 --eigen_index 2 --nu
+../simulation.py --chi 10 --eigen_index 2 --nu
+../simulation.py --chi 10 --eigen_index 2 --nu
+../simulation.py --chi 10 --eigen_index 2 --nu
+../simulation.py --chi 10 --eigen_index 2 --nu
+./genhtml.py
+
+../simulation.py --chi 30 --eigen_index 2 --nu
+../simulation.py --chi 30 --eigen_index 2 --nu
+../simulation.py --chi 30 --eigen_index 2 --nu
+../simulation.py --chi 30 --eigen_index 2 --nu
+../simulation.py --chi 30 --eigen_index 2 --nu
+../simulation.py --chi 30 --eigen_index 2 --nu
+./genhtml.py
+
+../simulation.py --chi 10 --eigen_index 2 --gamma
+../simulation.py --chi 10 --eigen_index 2 --gamma
+../simulation.py --chi 10 --eigen_index 2 --gamma
+../simulation.py --chi 10 --eigen_index 2 --gamma
+../simulation.py --chi 10 --eigen_index 2 --gamma
+../simulation.py --chi 10 --eigen_index 2 --gamma
+./genhtml.py
+
+../simulation.py --chi 30 --eigen_index 2 --gamma
+../simulation.py --chi 30 --eigen_index 2 --gamma
+../simulation.py --chi 30 --eigen_index 2 --gamma
+../simulation.py --chi 30 --eigen_index 2 --gamma
+../simulation.py --chi 30 --eigen_index 2 --gamma
+../simulation.py --chi 30 --eigen_index 2 --gamma
 ./genhtml.py
 
 ./upload.sh
