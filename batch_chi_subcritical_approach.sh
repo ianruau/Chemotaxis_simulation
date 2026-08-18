9536f191e644723f67a3072b97ee0ce18bc96d86
ianruau <iir0001@auburn.edu>
2026-08-18 14:47:30 -0500

    Numerical experiments to show unboundedness as chi_0\to\infty and numercal experiments to track branches in the subcritical case

---
+++ batch_chi_subcritical_approach.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# #!/bin/bash
+
+# All constants are 1, except for chi who starts at chi=23.9419
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.6000 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7600 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7620 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7640 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7655 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7660 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7673 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7674 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 95.7675 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 96.7680 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 96.7690 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 96.7705 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 96.7730 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 96.7760 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 96.8000 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 3 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 30 --eigen_index 2 --chi 96.8500 --time 100 --save_static_plots no --output_dir subcritical_approach --save_data no
---
+++ batch_increase_chi.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# #!/bin/bash
+
+# All constants are 1, except for chi who starts at chi=23.9419
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 23.9419 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 30 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 40 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 60 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 65 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 70 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 75 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 80 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 85 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 86 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 87 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 88 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 89 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 90 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 95 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 1 --gamma 1 --mu 1 --nu 1 --L 1 --meshsize_abs 50 --eigen_index 2 --chi 100 --time 100 --save_static_plots no --output_dir increasing_chi/eigen_index_2 --save_data no
