5bbac86aef5d6aeb16de61483aac74b755181a3d
ianruau <iir0001@auburn.edu>
2026-08-23 04:21:03 -0500

    New batches files added

---
+++ batch_chi_supercritical_approach.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# #!/bin/bash
+
+# All constants are 1, except for chi who starts at chi=23.9419
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8100 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8350 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8460 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8500 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8750 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8800 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8820 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8830 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8837 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8840 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8850 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8870 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8900 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.8950 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
+./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.01 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.9000 --time 1000 --save_static_plots no --output_dir supercritical_approach --save_data no
+# ./simulation.py --a 1 --b 1 --alpha 1 --m 1 --beta 2 --gamma 1 --mu 1 --nu 1 --L 1 --epsilon 0.5 --meshsize_abs 30 --eigen_mode_n 1 --chi 47.9100 --time 500 --save_static_plots no --output_dir supercritical_approach --save_data no
